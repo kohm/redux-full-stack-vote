b9e33529227ffea3d7a30b06427b28ffe461bda4
Lucio Poveda <lucio.poveda@gmail.com>
2017-05-30 17:41:03 -0300

    adding route component and results component

--- client/test/components/Voting_spec.js
+++ client/test/components/Voting_spec.js
@@ -1,10 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {
-  renderIntoDocument,
-  scryRenderedDOMComponentsWithTag,
-  Simulate
-} from 'react-addons-test-utils';
+import { renderIntoDocument, scryRenderedDOMComponentsWithTag, Simulate } from 'react-addons-test-utils';
 import {List} from 'immutable';
 import Voting from '../../src/components/Voting';
 import {expect} from 'chai';
